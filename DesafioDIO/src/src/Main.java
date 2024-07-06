e2252f16f1529519cebf083eb79a47cc2c3f25c6
CarolineSMachado <caroline.machadobz@gmail.com>
2024-07-05 22:39:17 -0300

    Atualizações recentes no Dev e Main

--- DesafioDIO/src/src/Main.java
+++ DesafioDIO/src/src/Main.java
@@ -2,7 +2,9 @@ package src;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -26,6 +28,32 @@ public class Main {
 		mentoria.setData(LocalDate.now());
 		System.out.println(mentoria);
 
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Backend Java");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		System.out.println("------------------");
+		
+		Dev dev1 = new Dev();
+		dev1.setNome("Caroline");
+		dev1.inscriverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos" + dev1.getConteudosInscritos());
+		dev1.progredir();
+		System.out.println("Conteúdos concluidos" + dev1.getConteudosConcluidos());
+		System.out.println("XP: " + dev1.calcularTotalXp());
+		
+		System.out.println("------------------");
+		
+		Dev dev2 = new Dev();
+		dev2.setNome("Simão");
+		dev2.inscriverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos" + dev2.getConteudosInscritos());
+		dev2.progredir();
+		System.out.println("Conteúdos concluidos" + dev2.getConteudosConcluidos());
+		System.out.println("XP: " + dev2.calcularTotalXp());
 	}
 
 }
